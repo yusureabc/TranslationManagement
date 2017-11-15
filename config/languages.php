a843643f8ba79bfdaf581758095f7ac0b66baf2f
Yusure <yushuai@yeelight.com>
2017-11-15 15:35:12 +0800

    增加需要翻译的语言

--- config/languages.php
+++ config/languages.php
@@ -2,15 +2,23 @@
 
 return [
     /* 英语 */
-    'en' => 'English',
+    'en'    => 'English',
     /* 韩语 */
-    'ko' => 'Korean',
+    'ko'    => 'Korean',
     /* 法语 */
-    'fr' => 'French',
+    'fr'    => 'French',
     /* 德语 */
-    'de' => 'German',
+    'de'    => 'German',
     /* 俄语 */
-    'ru' => 'Russian',
+    'ru'    => 'Russian',
     /* 西班牙语 */
-    'es' => 'Spanish',
+    'es'    => 'Spanish',
+    /* 中文(香港) */
+    'zh-HK' => 'Hongkong',
+    /* 中文(台湾) */
+    'zh-TW' => 'Taiwan',
+    /* 乌克兰语 */
+    'uk'    => 'Ukrainian',
+    /* 荷兰语 */
+    'nl'    => 'Dutch',
 ];
--- resources/lang/en/languages.php
+++ resources/lang/en/languages.php
@@ -13,4 +13,12 @@ return [
     'ru' => 'Russian',
     /* 西班牙语 */
     'es' => 'Spanish',
+    /* 中文(香港) */
+    'zh-HK' => 'Hongkong',
+    /* 中文(台湾) */
+    'zh-TW' => 'Taiwan',
+    /* 乌克兰语 */
+    'uk'    => 'Ukrainian',
+    /* 荷兰语 */
+    'nl'    => 'Dutch',
 ];
