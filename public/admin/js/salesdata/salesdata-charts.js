758e27f8c5fca1bf9c9b5fccf4f8cf46207780de
Sure Yu <yushuai@yeelight.com>
2018-07-17 10:55:06 +0800

    clean old file

--- public/admin/js/salesdata/salesdata-charts.js
+++
@@ -1,7 +0,0 @@
-$('#charts_show').on('click',function () {
-    var data_time_start = $("#data_time [name='data_time_start']").val();
-    var data_time_end = $("#data_time [name='data_time_end']").val();
-    var platform_id = $("#platform_id [name='platform_id']").val();
-    var product_id = $("#product_id [name='product_id']").val();
-    window.location.href = '/admin/salesdata/charts?' + 'platform_id=' + platform_id + '&product_id=' + product_id + '&data_time_start=' + data_time_start + '&data_time_end=' + data_time_end;
-});
