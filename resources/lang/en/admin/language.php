bb5c9de5e353718b816ef6d393abb5a3e20f5ba9
yusure <czhyusure@163.com>
2019-02-25 14:27:45 +0800

    translate text

--- resources/lang/en/admin/language.php
+++ resources/lang/en/admin/language.php
@@ -1,28 +1,17 @@
 <?php
+
 return [
-    'title'     => '语言管理',
-    'desc'      => '语言列表',
-    'create'    => '添加语言',
-    'edit'      => '修改语言',
-    'info'      => '语言信息',
+    'title'     => 'Language Management',
+    'desc'      => 'Language list',
+    'info'      => 'Language infomation',
     'model'     => [
-        'id'          => 'ID',
-        'name'        => '语言名称',
-        'slug'        => '语言标识',
-        'founder'     => '创建者',
-        'url'         => '语言网址',
-        'description' => '描述',
-        'sort'        => '排序',
-        'translation_language' => '待翻译语言',
-        'language'    => '语言代码',
-        'status'      => '状态',
-        'created_at'  => '创建时间',
-        'updated_at'  => '修改时间',
-        'submit_at'   => '提交时间',
-        'download_at' => '下载时间',
-    ],
-    'action' => [
-        'create' => '<i class="fa fa-plus"></i> 添加语言',
+        'id'                   => 'ID',
+        'name'                 => 'Language name',
+        'translation_language' => 'Language to be translated',
+        'language'             => 'Language code',
+        'status'               => 'Status',
+        'submit_at'            => 'Submission time',
+        'download_at'          => 'Download time',
     ],
 
 ];
