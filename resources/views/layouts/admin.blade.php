3b71b734afe5cfbe26f5c964e13b2d0348b4cd90
yusure <czhyusure@163.com>
2019-02-25 17:10:42 +0800

    Add manual link

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -29,48 +29,11 @@
               <li>
                   <span class="m-r-sm text-muted welcome-message">{{getUser()->name}}</span>
               </li>
-              <li class="dropdown">
-                  <a class="dropdown-toggle count-info" data-toggle="dropdown" href="#">
-                      <i class="fa fa-bell"></i>  <span class="label label-primary"></span>
-                  </a>
-                  {{--<ul class="dropdown-menu dropdown-alerts">
-                      <li>
-                          <a href="mailbox.html">
-                              <div>
-                                  <i class="fa fa-envelope fa-fw"></i> You have 16 messages
-                                  <span class="pull-right text-muted small">4 minutes ago</span>
-                              </div>
-                          </a>
-                      </li>
-                      <li class="divider"></li>
-                      <li>
-                          <a href="profile.html">
-                              <div>
-                                  <i class="fa fa-twitter fa-fw"></i> 3 New Followers
-                                  <span class="pull-right text-muted small">12 minutes ago</span>
-                              </div>
-                          </a>
-                      </li>
-                      <li class="divider"></li>
-                      <li>
-                          <a href="grid_options.html">
-                              <div>
-                                  <i class="fa fa-upload fa-fw"></i> Server Rebooted
-                                  <span class="pull-right text-muted small">4 minutes ago</span>
-                              </div>
-                          </a>
-                      </li>
-                      <li class="divider"></li>
-                      <li>
-                          <div class="text-center link-block">
-                              <a href="notifications.html">
-                                  <strong>See All Alerts</strong>
-                                  <i class="fa fa-angle-right"></i>
-                              </a>
-                          </div>
-                      </li>
-                  </ul>--}}
+
+              <li>
+                  <a href="http://confluence.yeedev.com/pages/viewpage.action?pageId=11698532" target="_blank"> Manual </a>
               </li>
+
               <li>
                   <a href="{{ url('logout') }}" onclick="event.preventDefault();document.getElementById('logout-form').submit();">
                       <i class="fa fa-sign-out"></i> Logout
